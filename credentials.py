db35bc09a6f895c13098971eb7d774c2e970d84d
milyun <38059969+milyun@users.noreply.github.com>
2021-10-04 16:13:18 +0700

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Go to http://apps.twitter.com and create an app.
+# The consumer key and secret will be generated for you
+API_KEY = "AoPoGKo5Qwbr93uPBsdKincpC"
+API_SECRET_KEY = "wCWT8CXlTjz1z6LayIiKqZL7fIfwdOgsumjNjS1ZiQnER5aoii"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+ACCESS_TOEKN = "1436510661161275392-3GYBYAZiKoTNjUEiOk9W6B9bTGFMdI"
+ACCESS_TOKEN_SECRET = "xDfsidDl6wSKjJ8ssaT1rAofoZTwsay7UmSuwSLorZdW9"
